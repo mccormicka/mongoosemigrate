bec6ac2b77edfc75b702e0cee9b3216f37be185e
Anthony McCormick <=>
2013-10-12 16:44:50 +0100

    Working up/down migrations

--- index.js
+++ index.js
@@ -2,45 +2,40 @@
 
 var logger = require('nodelogger').Logger(__filename);
 
-module.exports =    function MongooseMigration(mongoose, config) {
-    if(!config || !config.host || !config.db || !config.migrations ){
-        throw new Error('You must pass a configuration object with the follow params {\nhost:hostname, db:dbname, migrations:migrations location }');
-    }
-    setupMigrations(mongoose, config);
-};
-
-function setupMigrations(mongoose, config){
-
-    /**
-     * Migrate a database upwards using the config.
-     * @param config
-     */
-    mongoose.migrateDatabaseUp = function migrateDatabaseUp(migrateUpTo, callback) {
-        var migrate = connectToMigrations();
-        logger.info('Starting Migration: UP');
-        migrate.run('up', migrateUpTo, callback);
-    };
-
-    /**
-     * Migrate a database downwards using the config.
-     * @param config
-     */
-    mongoose.migrateDatabaseDown = function migrateDatabaseDown(rollBackTo, callback) {
-        var migrate = connectToMigrations();
-        logger.info('Starting Migrations: Down');
-        migrate.run('down', rollBackTo, callback);
-    };
-
-//-------------------------------------------------------------------------
+module.exports = require('./lib/MongooseMigrate');
 //
-// Private Methods
+//function setupMigrations(mongoose, config){
 //
-//-------------------------------------------------------------------------
-
-    function connectToMigrations() {
-        var migrate = require('mongo-migrate');
-        migrate.setConfigObject(config);
-        migrate.changeWorkingDirectory(config.migrations);
-        return migrate;
-    }
-}
+//    /**
+//     * Migrate a database upwards using the config.
+//     * @param config
+//     */
+//    mongoose.migrateDatabaseUp = function migrateDatabaseUp(migrateUpTo, callback) {
+//        var migrate = connectToMigrations();
+//        logger.info('Starting Migration: UP');
+//        migrate.run('up', migrateUpTo, callback);
+//    };
+//
+//    /**
+//     * Migrate a database downwards using the config.
+//     * @param config
+//     */
+//    mongoose.migrateDatabaseDown = function migrateDatabaseDown(rollBackTo, callback) {
+//        var migrate = connectToMigrations();
+//        logger.info('Starting Migrations: Down');
+//        migrate.run('down', rollBackTo, callback);
+//    };
+//
+////-------------------------------------------------------------------------
+////
+//// Private Methods
+////
+////-------------------------------------------------------------------------
+//
+//    function connectToMigrations() {
+//        var migrate = require('mongo-migrate');
+//        migrate.setConfigObject(config);
+//        migrate.changeWorkingDirectory(config.migrations);
+//        return migrate;
+//    }
+//}
--- test/Index.spec.js
+++ test/Index.spec.js
@@ -4,29 +4,107 @@ describe('SHOULD', function () {
 
     var mockgoose = require('Mockgoose');
     var Mongoose = require('mongoose').Mongoose;
-    var migrations = require('../index');
+    var MongooseMigrate = require('../index');
     var mongoose;
+    var migrate;
+
     beforeEach(function (done) {
         mongoose = new Mongoose();
-        mockgoose(mongoose);
-        migrations(mongoose, {
+//        mockgoose(mongoose);
+        migrate = new MongooseMigrate(mongoose, {
             host: 'localhost',
-            db: 'mongoose-migrate-test',
-            migrations: './test'
+            db: 'mongoose-migrate-test'
         });
-        mongoose.migrateDatabaseDown(undefined, function () {
-            mongoose.migrateDatabaseUp(undefined, done);
+        migrate.migrateDatabaseDown(function () {
+            migrate.migrateDatabaseUp(done);
         });
 
     });
 
     afterEach(function (done) {
-        mongoose.migrateDatabaseDown(null, done);
+        migrate.migrateDatabaseDown(done);
     });
 
     it('Item should be added to the database', function (done) {
-//        var model;// = mongoose.model('testcollection');
-//        expect(model).toBeDefined();
-//        done();
+        var Model = mongoose.model('testcollection');
+        expect(Model).toBeDefined();
+        done();
+    });
+
+    it('Item should have migrated values', function (done) {
+        var Model = mongoose.model('testcollection');
+        Model.findOne({name: 'John'}, function (err, result) {
+            expect(err).toBeNull();
+            expect(result).toBeDefined();
+            if (result) {
+                expect(result.name).toBe('John');
+            }
+            done(err);
+        });
+    });
+
+    it('Add Migrations to MigrationTable in Mongoose', function (done) {
+        var Migrations = mongoose.model('Migrations');
+        expect(Migrations).toBeDefined();
+        if (Migrations) {
+            Migrations.find({}, function (err, results) {
+                expect(err).toBeNull();
+                expect(results).toBeDefined();
+                if (results) {
+                    expect(results.length).toBe(2);
+                    done(err);
+                } else {
+                    done('Error retrieving migrations');
+                }
+            });
+        } else {
+            done('Error creating migrations model');
+        }
+    });
+
+    it('Items should be run in order', function (done) {
+        var Migrations = mongoose.model('Migrations');
+        expect(Migrations).toBeDefined();
+        if (Migrations) {
+            Migrations.find({}, {}, {sort: {index: -1}}, function (err, results) {
+                expect(err).toBeNull();
+                expect(results).toBeDefined();
+                if (results) {
+                    expect(results[0].index > results[1].index).toBe(true);
+                    done(err);
+                } else {
+                    done('Error retrieving migrations');
+                }
+            });
+        } else {
+            done('Error creating migrations model');
+        }
+    });
+
+    it('Tear down migrations', function (done) {
+        migrate.migrateDatabaseUp(function () {
+            var Migrations = mongoose.model('Migrations');
+            var Model = mongoose.model('testcollection');
+            expect(Migrations).toBeDefined();
+            expect(Model).toBeDefined();
+            migrate.migrateDatabaseDown(function () {
+                Migrations.find({}, function (err, migrations) {
+                    expect(migrations).toEqual([]);
+                    Model.find({}, function (err, models) {
+                        expect(models).toEqual([]);
+                        done(err);
+                    });
+                });
+            });
+        });
+    });
+
+    it('Should not load files that do not match the glob pattern', function (done) {
+        var Migrations = mongoose.model('Migrations');
+        Migrations.count(function(err, count){
+            expect(err).toBeNull();
+            expect(count).toBe(2);
+            done(err);
+        });
     });
 });
